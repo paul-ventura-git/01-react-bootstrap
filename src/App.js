8fbb885b9e25e1d777d72c9c0312e93030241af9
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-01 22:10:54 -0500

    World map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import NavScrollExample from "./components/MainNavbar";
 import ResponsiveTable from './components/ResponsiveTable';
 import MainForm from "./components/MainForm";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
+import "./styles.css";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">      
       <NavScrollExample></NavScrollExample>      
       <MainForm></MainForm>
       <ResponsiveTable></ResponsiveTable>
+      <Container className='d-flex justify-content-between'>
+        <ComposableMap>
+          <Geographies geography="/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </Container>
 
     </div>
   );
